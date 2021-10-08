672e2f408039c97589301bcb607bae04f5cd405a
Petinger <lucaswinpetzinger@gmail.com>
2021-10-08 15:38:30 -0300

    exercicio 1

---
+++ fundamentos/bloco-04-introducao-ao-javaScript-e-logica-de-programacao/dia-1-introducao-ao-java-script/exercicio1/scripts.js
@@ -0,0 +1,19 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+    firstName: 'Ana',
+    lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo, typeof patientEmail);
+//exercicios:
+
+const base = 5;
+let height = 8;
+const area = base * height;
+console.log(area);
+const permieter = base + height + base + height;
+console.log(permieter)
