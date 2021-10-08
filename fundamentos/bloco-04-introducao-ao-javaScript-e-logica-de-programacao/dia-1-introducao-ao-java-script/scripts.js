6c1ae648352d2389cc472ab7d609d1b3104b4539
Petinger <lucaswinpetzinger@gmail.com>
2021-10-08 15:21:00 -0300

    exercicios 4.1 course

---
+++ fundamentos/bloco-04-introducao-ao-javaScript-e-logica-de-programacao/dia-1-introducao-ao-java-script/scripts.js
@@ -0,0 +1,25 @@
+const myName = "Lucas";
+const birthCity = "Sorocaba";
+
+let birthYear = 2030;
+
+birthYear = 2021;
+console.log(myName, birthCity, birthYear);
+
+let movie = "Avangers"; //string literal
+let score = 10.5; //number literal
+let isValid = true; //boolean
+let name; //undefined
+let color = null; //redefinition
+//+, -, *, /, **
+
+let salary = 3500;
+
+console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary * salary);
+console.log(salary / salary);
+
+salary++;
+
+console.log(salary);
