510f2475c1ca2ae3e3d8777e965dae6aed0bb4f4
Petinger <lucaswinpetzinger@gmail.com>
2021-10-13 18:29:09 -0300

    exercicios-4.2-Mega-Sena

--- fundamentos/bloco-04-introducao-ao-javaScript-e-logica-de-programacao/dia-2-array-for-for-of/scripts.js
+++ fundamentos/bloco-04-introducao-ao-javaScript-e-logica-de-programacao/dia-2-array-for-for-of/scripts.js
@@ -1,9 +1,9 @@
-let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+let tasksList = ["Tomar café", "Reunião", "Brincar com o cachorro"];
 
 console.log(tasksList.length);
 // 3
 
-let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+let tasksList = ["Tomar café", "Reunião", "Brincar com o cachorro"];
 
 let searchForFirstTask = tasksList[0];
 console.log(searchForFirstTask);
@@ -13,75 +13,72 @@ let searchForLastTask = tasksList[tasksList.length - 1];
 console.log(searchForLastTask);
 // Brincar com o cachorro
 
-let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+let tasksList = ["Tomar café", "Reunião", "Brincar com o cachorro"];
 
-tasksList.push('Fazer exercícios da Trybe'); // adiciona mais uma tarefa
+tasksList.push("Fazer exercícios da Trybe"); // adiciona mais uma tarefa
 console.log(tasksList);
 
 // ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
 tasksList.unshift("pegar um cafezao");
-console.log(tasksList)
+console.log(tasksList);
 
-let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+let tasksList = ["Tomar café", "Reunião", "Brincar com o cachorro"];
 
 tasksList.shift(); // remove a última tarefa
 console.log(tasksList);
 
 // [ 'Tomar café', 'Reunião' ]
 
-let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+let tasksList = ["Tomar café", "Reunião", "Brincar com o cachorro"];
 
-let indexOfTask = tasksList.indexOf('Reunião');
+let indexOfTask = tasksList.indexOf("Reunião");
 console.log(indexOfTask);
 
 // 1
 
-
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[1]
+let menu = ["Home", "Serviços", "Portfólio", "Links"];
+let menuServices = menu[1];
 
 console.log(menuServices);
 
-let indexOfPortfolio = menu.indexOf('Portfólio');
+let indexOfPortfolio = menu.indexOf("Portfólio");
 
 console.log(indexOfPortfolio);
 
-menu.push('Contato')
-console.log(menu)
+menu.push("Contato");
+console.log(menu);
 
 ///FOR//
 
-let numero = 3
+let numero = 3;
 
 for (let contador = 1; contador <= 10; contador++) {
-    console.log('*')
-    console.log(numero * contador)
+    console.log("*");
+    console.log(numero * contador);
 }
 ///
 
-let listaDeNomes = ['lucas', 'thai', 'cafe', 'bolinha']
+let listaDeNomes = ["lucas", "thai", "cafe", "bolinha"];
 
 for (let index = 0; index < listaDeNomes.length; index += 1) {
     let mensagem = "Bem vindo " + listaDeNomes[index] + "!";
-    console.log(mensagem)
+    console.log(mensagem);
 }
 //
 
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
 
 for (let index = 0; index < groceryList.length; index += 1) {
     let lista = groceryList[index];
-    console.log(lista)
+    console.log(lista);
 }
-
 
 ///forof
 
-let word = 'Hello';
+let word = "Hello";
 for (let letter of word) {
     console.log(letter);
 }
-
 
 let arrOfNumbers = [10, 20, 30];
 for (let sum of arrOfNumbers) {
@@ -92,8 +89,30 @@ for (let sum of arrOfNumbers) {
 // 21
 // 31
 
-
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+let names = ["João", "Maria", "Antônio", "Margarida"];
 for (let name of names) {
-    console.log(name)
+    console.log(name);
+}
+
+///MEGA-SENA
+
+let n1 = Math.ceil(Math.random() * 60);
+let n2 = Math.ceil(Math.random() * 60);
+let n3 = Math.ceil(Math.random() * 60);
+let n4 = Math.ceil(Math.random() * 60);
+let n5 = Math.ceil(Math.random() * 60);
+let n6 = Math.ceil(Math.random() * 60);
+
+let megaSenaNumbers = [n1, n2, n3, n4, n5, n6];
+let myNumbers = [10, 20, 30, 40, 50, 60];
+
+for (let i = 0; i < megaSenaNumbers.length; i++) {
+    if (myNumbers[0] == megaSenaNumbers[i]) {
+        console.log("acertou");
+    } else {
+        console.log("não acertou");
+        console.log("meus numeros: " + myNumbers)
+        console.log("Sorteados: " + megaSenaNumbers)
+    }
+
 }
