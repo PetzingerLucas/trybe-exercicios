46619bfd9d2651c21fe740b41009c41d005c4ca1
Petinger <lucaswinpetzinger@gmail.com>
2021-10-29 14:40:21 -0300

    update exercicios

---
+++ fundamentos/bloco-05-javaScript-DOM-eventos-e-web-storage/dia-4-web-storage/Praticando2/script.js
@@ -0,0 +1,77 @@
+const button = document.getElementById('add-button');
+const input = document.getElementById('phrases-input');
+const list = document.getElementById('phrases-list');
+
+function addPhraseToLocalStorage() {
+  const oldList = JSON.parse(localStorage.getItem('phrases'));
+  const phraseText = input.value;
+  oldList.push(phraseText);
+  localStorage.setItem('phrases', JSON.stringify(oldList));
+  insertPhraseInDOM();
+};
+
+function insertPhraseInDOM() {
+  const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+  const listLength = phrasesList.length - 1;
+  const phraseText = phrasesList[listLength];
+  const phrase = document.createElement('li');
+  phrase.innerText = phraseText;
+  list.appendChild(phrase);
+};
+
+function initialRenderization() {
+  if (localStorage.getItem('phrases') === null) {
+    localStorage.setItem('phrases', JSON.stringify([]));
+  } else {
+    const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+    const listLength = phrasesList.length - 1;
+    for (let index = 0; index <= listLength; index += 1) {
+      const listElement = document.createElement('li');
+      listElement.innerText = phrasesList[index];
+      list.appendChild(listElement);
+    };
+  };
+};
+
+button.addEventListener('click', addPhraseToLocalStorage);
+
+window.onload = function() {
+  initialRenderization();
+};
+
+//---------------------------------------------------------------------------// 
+
+const button = document.getElementById('add-button');
+const input = document.getElementById('phrases-input');
+const list = document.getElementById('phrases-list');
+
+function addPhraseToSessionStorage() {
+  if (sessionStorage.getItem('phrases') === null) {
+    sessionStorage.setItem('phrases', JSON.stringify([]));
+  }
+  const oldList = JSON.parse(sessionStorage.getItem('phrases'));
+  const phraseText = input.value;
+  oldList.push(phraseText);
+  sessionStorage.setItem('phrases', JSON.stringify(oldList));
+  insertPhraseInDOM();
+};
+
+function insertPhraseInDOM() {
+  const phrasesList = JSON.parse(sessionStorage.getItem('phrases'));
+  const listLength = phrasesList.length - 1;
+  const phraseText = phrasesList[listLength];
+  const phrase = document.createElement('li');
+  phrase.innerText = phraseText;
+  list.appendChild(phrase);
+};
+
+button.addEventListener('click', addPhraseToSessionStorage);
+
+//---------------------------------------------------------------------------
+//utilizar o '.' ao invez de setItem para adicionar chave e valor no local e seassion storage//
+
+// Tudo o que é armazenado no local storage e seassion storage fica em forma de string.
+
+/// JSON.stringfy TRANSFORMA OBJETO EM STRING
+
+// JSON.parse transforma string estruturada em OBJETO
